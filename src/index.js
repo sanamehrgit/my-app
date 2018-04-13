2410864c0c0cb8ab86cffa6978191e78c84e85ed
sanamehrgit <webmail.sanamehr@gmail.com>
2018-04-13 20:42:17 +0430

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+﻿
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//ReactDOM.render(
+//    <h1>این یک نمونه برنامه ری اکت می باشد</h1>,
+//    document.getElementById('root')
+//)
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
